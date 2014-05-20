72872c2bfbc6a4eb5711786a0e2d3d29d86ca521
Cale Sennett <calesennett@gmail.com>
2014-05-19 19:04:25 -0500

    fix string concat

--- lib/dogecoin_api.rb
+++ lib/dogecoin_api.rb
@@ -39,7 +39,7 @@ module DogecoinApi
   private
 
   def self.call(endpoint)
-    response = HTTParty.get("#{@base_url + @api_key + "&a=" + endpoint}")
+    response = HTTParty.get(@base_url + @api_key + "&a=" + endpoint)
     response.body
   end
 end
